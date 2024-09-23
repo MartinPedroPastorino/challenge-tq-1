4abe4839fd503eed3ec926514a950a846e6b6400
SebastianRaiquenParisi <sebastian.parisi@tecval.com.ar>
2024-09-23 17:09:25 -0300

    commit inicial, repositorio iniciado con dependencias necesarias en pom.xml

---
+++ src/main/java/org/example/model/Superheroe.java
@@ -0,0 +1,11 @@
+package org.example.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class Superheroe {
+    private Integer id;
+    private String nombre;
+}
